d23984e809307b363757092781b6092291cc8bc6
swizkon <jonas@jerndin.se>
2020-09-27 22:25:41 +0200

    Test witha nother snotify component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,19 @@ export default {
     notify (event) {
       var _self = this
       _self.$snotify.success('Still works Example body content', 'Hello ' + event.target.innerText + '!')
+
+      _self.$snotify.confirm('Example body content', 'Example title', {
+        timeout: 5000,
+        showProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: true,
+        buttons: [
+          {text: 'Yes', action: () => console.log('Clicked: Yes'), bold: false},
+          {text: 'No', action: () => console.log('Clicked: No')},
+          {text: 'Later', action: (toast) => { console.log('Clicked: Later'); _self.$snotify.remove(toast.id) }},
+          {text: 'Close', action: (toast) => { console.log('Clicked: No'); _self.$snotify.remove(toast.id) }, bold: true}
+        ]
+      })
     }
   }
 }
